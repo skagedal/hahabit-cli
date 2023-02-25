e9c3f522d015c08809bd51afdd49d0e4172d0d1d
Simon Kågedal Reimer <skagedal@gmail.com>
2023-02-25 23:14:56 +0100

    Add Gradle package

--- generate-clients.sh
+++ generate-clients.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-JAVA_NATIVE_CLIENT_DIR=java-native-client
+BASE_PACKAGE=tech.skagedal.hahabit.generated
+JAVA_CLIENT_DIR=java-client
+TMP_DIR=$(mktemp -d)
+PKG_DIR=src/main/java/tech/skagedal/hahabit/generated
+SOURCE_DIR=$TMP_DIR/$PKG_DIR/generated
+TARGET_DIR=$JAVA_CLIENT_DIR/app/$PKG_DIR
 
-rm -rf $JAVA_NATIVE_CLIENT_DIR}
+rm -rf $JAVA_NATIVE_CLIENT_DIR
 openapi-generator generate \
     --input-spec ~/code/hahabit/openapi.yaml \
     --generator-name java \
     --library native \
-    --package-name tech.skagedal.hahabit.cli \
-    --output $JAVA_NATIVE_CLIENT_DIR
+    --api-package ${BASE_PACKAGE}.api \
+    --invoker-package ${BASE_PACKAGE}.invoker \
+    --package-name ${BASE_PACKAGE}.package \
+    --model-package ${BASE_PACKAGE}.model \
+    --output $TMP_DIR
 
+rm -rf $TARGET_DIR
+cp -r $SOURCE_DIR $TARGET_DIR/..
