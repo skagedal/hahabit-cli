642f27d2c6e32ef5bb14b89bf8eba1af776c1deb
Simon Kågedal Reimer <skagedal@gmail.com>
2023-03-01 20:15:03 +0100

    Add small install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd java-client
+
+TOOL=hahabit
+BIN=${HOME}/local/bin
+BUILT_BINARY=`pwd`/${TOOL}/build/install/${TOOL}/bin/${TOOL}
+
+./gradlew install
+ln -fs ${BUILT_BINARY} ${BIN}/${TOOL}
